d582ec5cbd76dd6d3ae7dd824cbf289f3f3efd40
Kykhoa-N <kn1029928@wcupa.edu>
2025-11-07 05:58:30 -0500

    Fix User print Fix adding duplicate user Fix user repo print

--- src/main/java/ecommerce/repo/UserRepo.java
+++ src/main/java/ecommerce/repo/UserRepo.java
@@ -1,8 +1,27 @@
 package ecommerce.repo;
 import ecommerce.model.User;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class UserRepo {
-    private Map<String, User> users_by_id;
+
+    private final Map<String, User> users_by_id = new HashMap<>();
+
+    public void add(User user) {
+        if(users_by_id.containsKey(user.getId())){
+            System.out.println("TRY AGAIN");
+        } else {
+            users_by_id.put(user.getId(), user);
+        }
+    }
+
+    @Override
+    public String toString() {
+        String output = "";
+        for(User user: users_by_id.values()) {
+            output += user + "\n";
+        }
+        return output;
+    }
 }
