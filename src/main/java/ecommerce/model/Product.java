c3908b69b76625502854db6a354505fa325786c6
Kykhoa-N <kn1029928@wcupa.edu>
2025-11-07 03:34:05 -0500

    Add field to Product model

--- src/main/java/ecommerce/model/Product.java
+++ src/main/java/ecommerce/model/Product.java
@@ -1,4 +1,9 @@
 package main.java.ecommerce.model;
 
 public class Product {
+    private String id;
+    private String name;
+    private String category;
+    private double price;
+    private int quantity;
 }
