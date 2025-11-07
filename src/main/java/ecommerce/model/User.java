19a9b39aaea332390a0a3ba1aa89f0c316dad566
Kykhoa-N <kn1029928@wcupa.edu>
2025-11-07 04:53:47 -0500

    User constructor

--- src/main/java/ecommerce/model/User.java
+++ src/main/java/ecommerce/model/User.java
@@ -4,4 +4,10 @@ public class User {
     private String id;
     private String name;
     private Role role;
+
+    public User(String id, String name, Role role) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+    }
 }
