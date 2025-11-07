993181a66b3b5f9b12de8da40d59747b740b5aa7
Kykhoa-N <kn1029928@wcupa.edu>
2025-11-07 03:58:24 -0500

    Add field to Cart model

--- src/main/java/ecommerce/model/Cart.java
+++ src/main/java/ecommerce/model/Cart.java
@@ -1,5 +1,8 @@
 package main.java.ecommerce.model;
+import java.util.Map;
 
 public class Cart {
-
+    private String userID;
+    private String address;
+    private Map<Integer, Integer> items;
 }
