74bcc435fadc8a69a38cf85ed749ffa8d1c6858b
Kykhoa-N <kn1029928@wcupa.edu>
2025-11-22 15:59:20 -0500

    Cart Model creation

--- src/main/java/ecommerce/model/Cart.java
+++ src/main/java/ecommerce/model/Cart.java
@@ -1,9 +1,33 @@
 package ecommerce.model;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Cart {
-    private String user_id;
+
+    // FIELD
+    private final String user_id;
     private Map<String, Integer> product_list;
-    private String address;
+    private final String address;
+
+    // CONSTRUCTOR
+    public Cart(String user_id, Map<String, Integer> product_list, String address) {
+        this.user_id = user_id;
+        this.product_list = new HashMap<>();
+        this.address = address;
+    }
+
+    // GETTER METHOD
+    public String getUserId() {
+        return user_id;
+    }
+
+    public Map<String, Integer> getProductlist() {
+        return product_list;
+    }
+    public String getAddress() {
+        return address;
+    }
+
+
 
 }
