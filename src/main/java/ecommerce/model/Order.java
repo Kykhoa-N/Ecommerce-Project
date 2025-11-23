5dd6fbe8d732a8fb4f2ec8dff6f68d24e83e5042
Kykhoa-N <kn1029928@wcupa.edu>
2025-11-23 13:17:02 -0500

    Order Model Creation

--- src/main/java/ecommerce/model/Order.java
+++ src/main/java/ecommerce/model/Order.java
@@ -2,10 +2,52 @@ package ecommerce.model;
 import java.util.Map;
 
 public class Order {
-    private String id;
-    private String user_id;
-    private Map<String, Integer> product_list;
-    private double total_price;
-    private int timestamp;
+
+    // FIELD
+    private final String id;
+    private final String user_id;
+    private final Map<String, Integer> product_list;
+    private final double total_price;
+    private final int timestamp;
     private OrderStatus status;
+
+    // CONSTRUCTOR
+    public Order(String id, String user_id, Map<String, Integer> product_list, double total_price, String userId, Map<String, Integer> productList, double totalPrice) {
+        this.id = id;
+        this.user_id = user_id;
+        this.product_list = product_list;
+        this.total_price = total_price;
+        timestamp = 0;
+        status = OrderStatus.PROCESSED;
+    }
+
+    // GETTER METHOD
+    public String getId() {
+        return id;
+    }
+
+    public  String getUserId() {
+        return user_id;
+    }
+
+    public Map<String, Integer> getProductList() {
+        return product_list;
+    }
+
+    public double getTotalPrice() {
+        return total_price;
+    }
+
+    public int getTimestamp() {
+        return timestamp;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    // SETTER METHOD
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
 }
