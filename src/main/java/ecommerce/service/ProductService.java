4f36c88a6e91c95794ef161d50a8fd27384c6543
Kykhoa-N <kn1029928@wcupa.edu>
2025-11-23 22:05:57 -0500

    product service skeleton

--- src/main/java/ecommerce/service/ProductService.java
+++ src/main/java/ecommerce/service/ProductService.java
@@ -4,4 +4,36 @@ import ecommerce.model.*;
 import ecommerce.repo.*;
 
 public class ProductService {
+
+    public boolean add() {
+        return false;
+    }
+
+    public boolean update() {
+        return false;
+    }
+
+    public boolean remove() {
+        return false;
+    }
+
+    public void search() {
+
+    }
+
+    public void viewAll() {
+
+    }
+
+    public void filterPrice() {
+
+    }
+
+    public boolean checkout() {
+        return false;
+    }
+
+    public void orderHistory() {
+        
+    }
 }
