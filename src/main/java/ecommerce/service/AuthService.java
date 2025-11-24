2f72f56539db88255d97873c616430a911fb81aa
Kykhoa-N <kn1029928@wcupa.edu>
2025-11-23 21:15:26 -0500

    Auth Service skeleton created

--- src/main/java/ecommerce/service/AuthService.java
+++ src/main/java/ecommerce/service/AuthService.java
@@ -1,4 +1,15 @@
 package ecommerce.service;
 
+import ecommerce.model.*;
+import ecommerce.repo.*;
+
 public class AuthService {
+
+    public boolean register() {
+        return false;
+    }
+
+    public boolean login() {
+        return false;
+    }
 }
