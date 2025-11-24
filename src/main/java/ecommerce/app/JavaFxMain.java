a6ec826ed0eb245604c8db6382d77768728b0606
Kykhoa-N <kn1029928@wcupa.edu>
2025-11-24 00:08:22 -0500

    JavaFxMain class created for JavaFx usage

---
+++ src/main/java/ecommerce/app/JavaFxMain.java
@@ -0,0 +1,26 @@
+package ecommerce.app;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class JavaFxMain extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Hello JavaFX!");
+        StackPane root = new StackPane(label);
+
+        Scene scene = new Scene(root, 400, 300);
+
+        stage.setTitle("Ecommerce JavaFX Test");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
